afdd23b2924fca5a98324114ed23dfba0bc5255c
Thomas Presicci <thomaspresicci@gmail.com>
2019-02-12 15:51:14 -0600

    Initial commit

---
+++ Lab4/src/Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-2-11
+OU ID_______________: XXXXXXXXX
+OU EMAIL____________: thomaspresicci@ou.edu
+
+Tasks:
+1. Finish Animal equals() methods
+2. Finish AnimalTest class
+3. Finish Equipment equals() method
+4. Implement EquipmentTest
+5. Implement ZooTest
+
+Estimated Time (minutes):
+1. 5
+2. 10
+3. 5
+4. 20
+5. 20
+
+Actual Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+*/}
