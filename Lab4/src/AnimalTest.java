3ad658974267a6c6f88420b2c3ca11fcca5c1039
Thomas Presicci <thomaspresicci@gmail.com>
2019-02-12 20:33:02 -0600

    Implemented fullConstructorTest() and toStringTest()

--- Lab4/src/AnimalTest.java
+++ Lab4/src/AnimalTest.java
@@ -23,5 +23,55 @@ public class AnimalTest
     }
 
     // TODO: test full constructor, getters, and toString
+    public void fullConstructorTest() throws AssertException
+    {
+    	String color1 = "Purple";
+    	String name1 = "Zebra";
+    	Double weight1 = 200.00;
+    	Double height1 = 2.00;
+    	
+    	String color2 = "Yellow";
+    	String name2 = "Dog";
+    	Double weight2 = 50.00;
+    	Double height2 = 1.00;
+    	
+    	String color3 = "Green";
+    	String name3 = "Rhino";
+    	Double weight3 = 600.00;
+    	Double height3 = 4.00;
+    	
+    	//String color, String name, double weight, double height
+    	Animal animal1 = new Animal(color1, name1, weight1, height1);
+    	Animal animal2 = new Animal(color2, name2, weight2, height2);
+    	Animal animal3 = new Animal(color3, name3, weight3, height3);
+    	
+    	Assert.assertTrue(animal1.getColor().equals(color1));
+    	Assert.assertTrue(animal1.getName().equals(name1));
+    	Assert.assertEquals(weight1, animal1.getWeight(), 0.01);
+    	Assert.assertEquals(height1, animal1.getHeight(), 0.01);
+    	
+    	Assert.assertTrue(animal2.getColor().equals(color2));
+    	Assert.assertTrue(animal2.getName().equals(name2));
+    	Assert.assertEquals(weight2, animal2.getWeight(), 0.01);
+    	Assert.assertEquals(height2, animal2.getHeight(), 0.01);
+    	
+    	Assert.assertTrue(animal3.getColor().equals(color3));
+    	Assert.assertTrue(animal3.getName().equals(name3));
+    	Assert.assertEquals(weight3, animal3.getWeight(), 0.01);
+    	Assert.assertEquals(height3, animal3.getHeight(), 0.01);
+    }
+    
+    public void toStringTest() throws AssertException
+    {
+    	String color = "Purple";
+    	String name = "Zebra";
+    	Double weight = 200.00;
+    	Double height = 2.00;
+    	
+    	Animal animal = new Animal(color, name, weight, height);
+    	
+    	Assert.assertEquals(String.format("%s, a %s-colored animal. %.1f pounds, %.1f inches\n",
+                name, color, weight, height), animal.toString());
+    }
 }
 
